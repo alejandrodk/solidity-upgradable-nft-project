7c8b1fe335365048416ce20715b95dc2fe591a04
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-15 20:25:04 -0300

    new: initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: process.env.SOLIDITY_VERSION || "0.8.17",
+};
+
+export default config;
