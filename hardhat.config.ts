6fbb239c1d46396f831bb971560c77635345fc5d
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-16 00:31:00 -0300

    update: config goerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,22 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import dotenv from "dotenv";
+import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
 
+const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
-  solidity: process.env.SOLIDITY_VERSION || "0.8.17",
+  solidity: process.env.SOLIDITY_VERSION || "0.8.4",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
