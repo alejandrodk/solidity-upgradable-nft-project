233606dc20a3abd01301a0438000b56b4bcb17bc
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-16 00:33:39 -0300

    update: config deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // Grab the contract factory
+  const XNFT = await ethers.getContractFactory("XNFT");
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  // Start deployment, returning a promise that resolves to a contract object
+  const xNFT = await XNFT.deploy(); // Instance of the contract
+  console.log("Contract deployed to address:", xNFT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
