b35d0edbac2cd8d855a1a42440f95ae305bc11e5
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-22 23:12:50 -0300

    feat: prevent dummy transfers

--- contracts/XNftV2.sol
+++ contracts/XNftV2.sol
@@ -10,22 +10,26 @@ contract XNftV2 is ERC1155URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _nftIds;
 
+    enum NftClass{ BACKGROUND, DUMMY, WEARABLE }
+    mapping(uint256 => NftClass) private _classes;
+
     string public name = "XNft-collection-V2";
     string public symbol = "XNftV2";
 
-    constructor() ERC1155("") {}
+    constructor() ERC1155 ("") {}
 
-    function mint(address recipient, string memory tokenURI)
-        public
-        onlyOwner
-        returns (uint256)
-    {
+    function mint(
+        address recipient, 
+        string memory tokenURI,
+        NftClass class
+    ) public onlyOwner returns (uint256) {
         _nftIds.increment();
 
         uint256 nftId = _nftIds.current();
-
+        
         _mint(recipient, nftId, 1, "");
         _setURI(nftId, tokenURI);
+        _classes[nftId] = class;
 
         return nftId;
     }
@@ -33,17 +37,40 @@ contract XNftV2 is ERC1155URIStorage, Ownable {
     function mintBatch(
         address recipient,
         uint256[] memory amounts,
-        string[] memory tokenURIs
+        string[] memory tokenURIs,
+        NftClass[] memory classes
     ) public onlyOwner {
         uint256[] memory nftIds;
-
-        for (uint256 i = 0; i < amounts.length; i++) {
+   
+        for(uint256 i = 0; i < amounts.length; i++) {
             _nftIds.increment();
             nftIds[i] = _nftIds.current();
-
+            
             _setURI(nftIds[i], tokenURIs[i]);
+            _classes[nftIds[i]] = classes[i];
         }
 
         _mintBatch(recipient, nftIds, amounts, "");
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(
+            operator,
+            from,
+            to,
+            ids,
+            amounts,
+            data
+        );
+        for(uint256 i = 0; i < ids.length;i++) {
+            require(_classes[ids[i]] != NftClass.DUMMY, "Unable to transfer dummy");
+        }
     }
 }
