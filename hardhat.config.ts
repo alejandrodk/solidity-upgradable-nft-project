ef4df41aaedb6bbf6f137081e493ed8a255d9e7f
Alejandro Barrios <ottoabarriosp@hotmail.com>
2022-09-25 11:21:42 -0300

    update: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
 
-const { ALCHEMY_API_URL, PRIVATE_KEY, ALCHEMY_NETWORK } = process.env;
+const { SOLIDITY_VERSION, ALCHEMY_API_URL, PRIVATE_KEY, ETH_NETWORK } =
+  process.env;
 
 const config: HardhatUserConfig = {
-  solidity: process.env.SOLIDITY_VERSION || "0.8.4",
-  defaultNetwork: ALCHEMY_NETWORK,
+  solidity: SOLIDITY_VERSION || "0.8.17",
+  defaultNetwork: ETH_NETWORK,
   networks: {
     hardhat: {},
     goerli: {
